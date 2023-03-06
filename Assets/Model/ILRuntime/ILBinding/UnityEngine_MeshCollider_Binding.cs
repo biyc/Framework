f9276f7b2fc845a628b50b6338b4e09a4a50ebd7
“biyc <1040800070@qq.com>
2023-03-06 20:26:13 +0800

    []

---
+++ Assets/Model/ILRuntime/ILBinding/UnityEngine_MeshCollider_Binding.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+using ILRuntime.CLR.TypeSystem;
+using ILRuntime.CLR.Method;
+using ILRuntime.Runtime.Enviorment;
+using ILRuntime.Runtime.Intepreter;
+using ILRuntime.Runtime.Stack;
+using ILRuntime.Reflection;
+using ILRuntime.CLR.Utils;
+
+namespace ILRuntime.Runtime.Generated
+{
+    unsafe class UnityEngine_MeshCollider_Binding
+    {
+        public static void Register(ILRuntime.Runtime.Enviorment.AppDomain app)
+        {
+            BindingFlags flag = BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.DeclaredOnly;
+            MethodBase method;
+            Type[] args;
+            Type type = typeof(UnityEngine.MeshCollider);
+            args = new Type[]{typeof(UnityEngine.MeshColliderCookingOptions)};
+            method = type.GetMethod("set_cookingOptions", flag, null, args, null);
+            app.RegisterCLRMethodRedirection(method, set_cookingOptions_0);
+
+
+        }
+
+
+        static StackObject* set_cookingOptions_0(ILIntepreter __intp, StackObject* __esp, IList<object> __mStack, CLRMethod __method, bool isNewObj)
+        {
+            ILRuntime.Runtime.Enviorment.AppDomain __domain = __intp.AppDomain;
+            StackObject* ptr_of_this_method;
+            StackObject* __ret = ILIntepreter.Minus(__esp, 2);
+
+            ptr_of_this_method = ILIntepreter.Minus(__esp, 1);
+            UnityEngine.MeshColliderCookingOptions @value = (UnityEngine.MeshColliderCookingOptions)typeof(UnityEngine.MeshColliderCookingOptions).CheckCLRTypes(StackObject.ToObject(ptr_of_this_method, __domain, __mStack));
+            __intp.Free(ptr_of_this_method);
+
+            ptr_of_this_method = ILIntepreter.Minus(__esp, 2);
+            UnityEngine.MeshCollider instance_of_this_method = (UnityEngine.MeshCollider)typeof(UnityEngine.MeshCollider).CheckCLRTypes(StackObject.ToObject(ptr_of_this_method, __domain, __mStack));
+            __intp.Free(ptr_of_this_method);
+
+            instance_of_this_method.cookingOptions = value;
+
+            return __ret;
+        }
+
+
+
+    }
+}
